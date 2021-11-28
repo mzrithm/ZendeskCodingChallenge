eef8d3340b051eaffa745b1c765a784cd8b9a495
mzrithm <74382959+mzrithm@users.noreply.github.com>
2021-11-28 12:05:58 -0800

    Completes comments and docstrings for ZenDeskCodingChallenge.py

--- ZenDeskCodingChallenge.py
+++ ZenDeskCodingChallenge.py
@@ -136,48 +136,51 @@ class ZendeskTicket:
         over the "tickets" data member if the "search_results" data member is not None.
         This method uses the "page_display" data member to determine tickets displayed per page.
         This method also prints descriptive messages for the user about what ticket ids are currently
-        being displayed, how many tickets in total are being displayed, and when there are no more tickets
-        to display. This method validates the presence of tickets in "tickets" data member and restores
-        default of None in "search_results" data member when it is called.
+        being displayed, how many tickets in total are being displayed, and when it is the end of the display.
+        This method validates the presence of tickets in "tickets" data member and restores
+        the default of None in "search_results" data member when it is called.
         """
         if self._tickets is not None and len(self._tickets) != 0:
             page = self._page_display
-            max_width = 70      # ticket display format
+            max_width = 70      # ticket display format for page width; default = 70
             count = 0
-            if self._search_results is None:    # give precedence to search_results over tickets
+            if self._search_results is None:    # give precedence to search_results over display of all tickets
                 tickets = self._tickets
             else:
                 tickets = self._search_results
             total = len(tickets)                # capture total number of tickets
             desired_fields = ["requester_id", "assignee_id", "subject", "description", "tags"]
-            ticket_break = max_width * "_"
+            # This list of desired_fields could be used to create another feature where the user adds
+            # fields to their ticket display. Given the tickets.json file for this challenge, I selected
+            # only those fields for the ticket display that were populated with (non-default) data.
+            ticket_break = max_width * "_"      # dynamically updates line break to match max_width
             print(ticket_break)
             for id in tickets:
-                print(f"[Ticket ID #{id}]")
+                print(f"[Ticket ID #{id}]")     # key value for python ticket dictionary
                 count += 1
                 for key in desired_fields:
-                    key_text = format_key_display(key)
+                    key_text = format_key_display(key)      # format presentation of each key for pretty printing
                     print(f"{key_text}", end="")
                     if type(tickets[id][key]) is str:
-                        if key == "description":
-                            print("\n", end="")
+                        if key == "description":            # handle the large text block of description
+                            print("\n", end="")             # by inserting a new line to preserve maxwidth
                         text = wrap(tickets[id][key], width=max_width)
                         for line in text:
                             print(line)
                         print("\n", end="")
-                    elif type(tickets[id][key]) is list:
+                    elif type(tickets[id][key]) is list:    # formats data that is received as a list; Ex. Tags
                         for item in tickets[id][key]:
-                            if item == list(tickets[id][key])[-1]:
-                                print(f"{item} ", end="")
+                            if item == list(tickets[id][key])[-1]:  # formats printing of list as comma separated values
+                                print(f"{item} ", end="")           # prevents comma placement at end of list
                             else:
                                 print(f"{item}, ", end="")
-                    else:
+                    else:                                   # not a string or a list, print the integer
                         print(tickets[id][key])
                 print("\n", "\n", end="")
-                print(self.add_API_timestamp(id))
+                print(self.add_API_timestamp(id))           # add timestamp of every API call to the ticket display
                 print(ticket_break)
-                id_list = list(tickets)
-                if count % page == 0:
+                id_list = list(tickets)                     # used to index ticket id numbers for descriptive user messages
+                if count % page == 0:                       # tracks the pagination of ticket displays
                     if page == 1:
                         print(f"Ticket #{id} is displayed out of {total} tickets.")
                     else:
@@ -194,34 +197,48 @@ class ZendeskTicket:
                         if user_input == 'q':
                             print("\n", end="")
                             return
-                if total < page and total == count:
-                    if total == 1:
+                if total < page and total == count:     # if fewer tickets are being displayed than the pagination value
+                    if total == 1:                      # and the last ticket has been printed, display message
                         print(f"There is {total} ticket displayed.")
                         print("\n", end="")
                     else:
                         print(f"There are {total} tickets displayed.")
                         print("\n", end="")
-            self._search_results = None
+            self._search_results = None                 # restore search_results data member to None
 
     def search_subject(self, search_term):
-        """docstring"""
+        """
+        This method accepts a search_term as a parameter and searches
+        all ticket subject fields for the search term. This method updates
+        the "search_results" data member with the search_results, if there are any,
+        otherwise it displays a descriptive message for the user and returns.
+        Given search_results, this method displays the number of tickets that
+        contain the search term in the subject field and calls display_tickets().
+        """
         search_results = {}
-        for ticket in self._tickets:
-            if search_term in self._tickets[ticket]["subject"].lower():
-                search_results[ticket] = self._tickets[ticket]
-        total = len(search_results)
+        for ticket in self._tickets:    # the search methods could be made to be modular by passing a parameter with a string
+            if search_term in self._tickets[ticket]["subject"].lower():  # for the desired search field
+                search_results[ticket] = self._tickets[ticket]           # given more time, that would be my choice
+        total = len(search_results)                                      # in addition to offering multiple field searches
         if total == 0:
             print(f"There are no results that match your subject search for: {search_term}")
         else:
             print(f"There are {total} tickets that match your subject search for: {search_term}")
             print("Press any key to display results.")
             input()
-            self._search_results = search_results
-            self.display_tickets()
+            self._search_results = search_results   # search_results have precedence so, only update data member if
+            self.display_tickets()                  # there is at least 1 ticket to display
 
     def search_description(self, search_term):
-        """docstring"""
-        search_results = {}
+        """
+        This method accepts a search_term as a parameter and searches
+        all ticket description fields for the search term. This method updates
+        the "search_results" data member with the search_results, if there are any,
+        otherwise it displays a descriptive message for the user and returns.
+        Given search_results, this method displays the number of tickets that
+        contain the search term in the description field and calls display_tickets().
+        """
+        search_results = {}             # clone of search_subject for description field
         for ticket in self._tickets:
             if search_term in self._tickets[ticket]["description"].lower():
                 search_results[ticket] = self._tickets[ticket]
@@ -232,31 +249,45 @@ class ZendeskTicket:
             print(f"There are {total} tickets that match your description search for: {search_term}")
             print("Press any key to display results.")
             input()
-            self._search_results = search_results
+            self._search_results = search_results   # only update data member if search_results exist
             self.display_tickets()
 
     def search_tags(self, search_term):
-        """docstring"""
+        """This method accepts a search_term as a parameter and searches
+        all ticket tags for the search term. This method updates
+        the "search_results" data member with the search_results, if there are any,
+        otherwise it displays a descriptive message for the user and returns.
+        Given search_results, this method displays the number of tickets that
+        contain the search term in the tags field and calls display_tickets()."""
         search_results = {}
         for ticket in self._tickets:
             if search_term in self._tickets[ticket]["tags"]:
-                search_results[ticket] = self._tickets[ticket]
-        total = len(search_results)
+                search_results[ticket] = self._tickets[ticket]      # construct dictionary output to fit parameters
+        total = len(search_results)                                 # of display_tickets()
         if total == 0:
             print(f"There are no results that match your tag search for: {search_term}")
         else:
             print(f"There are {total} tickets that match your tag search for: {search_term}")
             print("Press any key to display results.")
             input()
-            self._search_results = search_results
+            self._search_results = search_results   # only update data member if search_results exist
             self.display_tickets()
 
     def search_ticket_id(self, id_number):
-        """docstring"""
+        """
+        This method accepts an ID number as a parameter and searches
+        all tickets for the ID number. This method updates
+        the "search_results" data member with the search_results, if there are any,
+        otherwise it displays a descriptive message for the user and returns.
+        The id_number is validated in main() to fall in the range of available ticket IDs.
+        This choice of external validation allows for re-prompting of the user for valid input.
+        Given search_results, this method displays when the ticket has been found
+        and calls display_tickets().
+        """
         search_results = {}
         try:
             search_results[id_number] = self._tickets[id_number]
-        except KeyError:
+        except KeyError:        # should an id number be passed that triggers a dictionary KeyError
             print(f"Ticket #{id_number} cannot be found.")
         total = len(search_results)
         if total == 0:
@@ -265,14 +296,20 @@ class ZendeskTicket:
             print(f"Ticket #{id_number} has been retrieved.")
             print("Press any key to display results.")
             input()
-            self._search_results = search_results
+            self._search_results = search_results       # only update data member if search_results exist
             self.display_tickets()
 
 
 def format_key_display(key):
-    """docstring"""
-    offset = 15
-    length = len(key)
+    """
+    This function accepts a string as a parameter and formats
+    the string for pretty printing by the display_tickets() method
+    in the ZendeskTicket class. Formatting includes adding spacing offsets,
+    removing underscores, and adding a colon to the output.
+    Returns formatted string.
+    """
+    offset = 15         # offset value determines the indentation of all data following the printing
+    length = len(key)   # of category fields in the display_tickets() method; does not apply to "description"
     diff = offset - length
     space_string = diff * " "
     key = key.replace("_", " ")
@@ -283,8 +320,16 @@ def format_key_display(key):
 
 
 def display_menu():
-    low = 1
+    """
+    This function displays a menu of choices that are methods of the
+    ZendeskTicket class. This function is called by main().
+    This function validates the user_input, and reprompts the user for alternate
+    input if it cannot validate it. Returns the validated input for main()
+    or an additional call of display_menu() to reprompt the user.
+    """
+    low = 1         # values allow easy updating of int validation
     high = 8
+    print("\n", end="")
     print("*************************************")
     print("Welcome to the Zendesk Ticket Viewer!")
     print("*************************************")
@@ -300,9 +345,9 @@ def display_menu():
         8: "Change Number of Tickets Displayed Per Page",
         "Q": "Quit Zendesk Ticket Viewer"
     }
-    for key in menu:
-        print(f"{key} : {menu[key]}")
-    selection = input().strip()
+    for key in menu:                        # input validation could also be as a menu dictionary call
+        print(f"{key} : {menu[key]}")       # in the form of a Try: / Except KeyError:
+    selection = input().strip()             # where the KeyError causes a reprompt of the user
     if selection.lower() == "q":
         return selection
     elif selection.isnumeric() and low <= int(selection) <= high:
@@ -318,8 +363,8 @@ def display_menu():
 if __name__ == "__main__":
 
     user_engaged = True
-    zt = ZendeskTicket()
-    zt.get_tickets()
+    zt = ZendeskTicket()    # create ZendeskTicket object
+    zt.get_tickets()        # populate "tickets" data member with python dictionary of tickets
     while user_engaged:
         user_input = display_menu()     # user input is validated by display_menu()
         if user_input == 1:  # display tickets
@@ -329,19 +374,19 @@ if __name__ == "__main__":
         elif user_input == 3:  # search tickets by subject
             print("Please enter the * SUBJECT * search term.")
             search_term = input().strip().lower()
-            zt.search_subject(search_term)
+            zt.search_subject(search_term)  # this method calls display_tickets()
         elif user_input == 4:  # search tickets by description
             print("Please enter the * DESCRIPTION * search term.")
             search_term = input().strip().lower()
-            zt.search_description(search_term)
+            zt.search_description(search_term)  # this method calls display_tickets()
         elif user_input == 5:  # search tickets by tag identifier
-            zt.get_tags_info()
+            zt.get_tags_info()  # display the tags for reference
             print("Please enter the * TAGS * search term.")
             search_term = input().strip().lower()
-            zt.search_tags(search_term)
+            zt.search_tags(search_term)  # this method calls display_tickets()
         elif user_input == 6:  # search tickets by ticket id number
             ticket_ids = list(zt.get_ticket_data())
-            first_ticket = ticket_ids[0]
+            first_ticket = ticket_ids[0]    # used to validate ticket id number
             last_ticket = ticket_ids[-1]
             searching = True
             while searching:
@@ -349,12 +394,14 @@ if __name__ == "__main__":
                 print(f"Please enter the * TICKET ID * number.")
                 id_number = input().strip()
                 if id_number.isnumeric() and first_ticket <= int(id_number) <= last_ticket:
-                    searching = False
-                    zt.search_ticket_id(int(id_number))
+                    searching = False       # if ticket id number is valid, retrieve ticket
+                    zt.search_ticket_id(int(id_number)) # this method calls display_tickets()
                 elif id_number.lower() == "q":
                     searching = False
                 else:
-                    print("Invalid entry. Please try again or enter 'Q' to quit.")
+                    print("\n", end="")
+                    print("Invalid entry. Please try again or enter 'Q' to quit.")  # reprompt user
+                    print("\n", end="")
         elif user_input == 7:  # update local tickets from zendesk api
             zt.get_tickets()
             print("* Your Zendesk API call was successful! *")
@@ -368,6 +415,6 @@ if __name__ == "__main__":
             print("\n", end="")
             print(f"* You will see {zt.get_page_display()} ticket(s) per page. *")
             print("\n", end="")
-        else:  # quit program
+        else:  # quit program; display_menu() validates data so this is the user selecting q or Q
             user_engaged = False
             print("Thank you for using the Zendesk Ticket Viewer. Goodbye!")
